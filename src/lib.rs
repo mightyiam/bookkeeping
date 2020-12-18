af46a6da48d29df0d98afc1a7d9fa8ec4cf34e17
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2020-12-18 17:48:24 +0700

    move readme to crate-level-doc and tutorial module

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,59 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
-#![cfg_attr(test, deny(missing_docs))]
+//#![deny(missing_docs)]
 #![deny(broken_intra_doc_links)]
 //#![deny(private_intra_doc_links)]
-#![cfg_attr(test, deny(missing_crate_level_docs))]
+//#![deny(missing_crate_level_docs)]
 //#![deny(missing_doc_code_examples)]
 //#![deny(private_doc_tests)]
 #![deny(invalid_codeblock_attributes)]
 #![doc(test(attr(deny(warnings))))]
-#![cfg_attr(test, feature(external_doc))]
 #![deny(clippy::all)]
-#![cfg_attr(test, doc(include = "../readme.md"))]
+//! ![GitHub Workflow Status (branch)][ci]
+//!
+//! This crate tries to model the very basics of the [bookkeeping][bookkeeping] activity.
+//! _it is a new rustacean's first open source crate_.
+//!
+//! ## Outline
+//!
+//! A book contains
+//! - accounts,
+//! - units (may represent currencies)
+//! - and transactions, which in turn, contain moves.
+//!
+//! ## Features
+//!
+//! - Book balance guaranteed at compile time.
+//! - Arbitrary metadata may be stored in books, accounts, units, transactions and moves.
+//!
+//! ## Defficiencies
+//! - No optimization of balance calculations.
+//!
+//! [ci]: https://img.shields.io/github/workflow/status/mightyiam/bookkeeping/Rust/master?logo=github
+//! [bookkeeping]: https://en.wikipedia.org/wiki/Bookkeeping
+//!
+//! ## Tutorial
+//!
+//! [Here][mod@tutorial].
+//!
+//! ## FAQ
+//!
+//! > This API can panic in a bunch of places. I don't like that. I don't feel safe.
+//! > How about returning `Result`s, instead?
+//!
+//! `Result`s and errors are for when a function might fail despite all caution.
+//! In this crate, panics would only occur on wrong usage.
+//! Having this crate return `Result`s would complicate the API and — worse —
+//! would give the impression that function calls could fail even when used correctly.
+//! I'd like the user to be confident that with correct usage the API is safe.
 #[macro_use]
 mod macros;
+macro_rules! tutorial {
+    ($doc:expr) => {
+        #[doc = $doc]
+        pub mod tutorial {}
+    };
+}
+tutorial!(include_str!("../tutorial.md"));
 mod account;
 mod balance;
 mod book;
